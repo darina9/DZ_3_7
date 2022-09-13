9e8856c0efc714e1137b928cb991a17c852e69d9
darina9 <krastinadv@gmail.com>
2022-09-13 20:11:08 +0300

    Zadacha_47

---
+++ Zadacha_47/Program.cs
@@ -0,0 +1,13 @@
+﻿int rows= Convert.ToInt32(Console.ReadLine());
+int columns= Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = new double[rows,columns];
+for(int i=0; i<matrix.GetLength(0); i++)
+{
+    for(int j=0; j<matrix.GetLength(1); j++)
+    {
+        matrix[i,j]= new Random().Next(-100,100)/10.0;
+        Console.Write(matrix[i,j]+"\t");
+    }
+    Console.WriteLine();
+}
