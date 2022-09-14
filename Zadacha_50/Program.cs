51c62f6f4bc059c76ae65c05fd4c5e7adc450c57
darina9 <krastinadv@gmail.com>
2022-09-14 16:51:10 +0300

    Zadacha_50

---
+++ Zadacha_50/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("Введите размеры массива");
+int rows= Convert.ToInt32(Console.ReadLine());
+int columns= Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[rows,columns];
+for (int i = 0; i < matrix.GetLength(0); i++) 
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {    
+        matrix [i, j] = Convert.ToInt32(new Random().Next(0,21)); 
+        Console.Write(matrix[i,j] + "\t  "); 
+}
+Console.WriteLine();
+}
+  
+ Console.WriteLine("Введите координаты");
+ int a = Convert.ToInt32(Console.ReadLine());
+ int b = Convert.ToInt32(Console.ReadLine());
+ if (a>rows || b>columns)
+ { 
+    Console.WriteLine("такого числа нет");}
+ else
+ { 
+    Console.WriteLine(matrix[a-1,b-1]);}
